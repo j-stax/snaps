6127ae2034c3c2fd63ae2a1bf7104039561d0005
j-stax <ahnjames01@gmail.com>
2025-02-07 19:44:58 -0500

    Navbar.jsx - add hover effect on filter button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,37 @@
 import './Navbar.scss'
-import filter from '../assets/images/Filter.svg'
+import filterIcon from '../assets/images/Filter.svg'
+import { ReactSVG } from "react-svg"
+import { useState, useRef, useEffect } from 'react'
 
 export default function Navbar() {
+    const [isHovered, setIsHovered] = useState(false)
+    const filterRef = useRef(null)
+
+    function handleMouseOver() {
+        setIsHovered(true)
+        const filterElemPath = filterRef.current.reactWrapper.querySelector("path")
+        filterElemPath.classList.add("header__filter-icon-hovered")
+    }
+
+    function handleMouseOut() {
+        setIsHovered(false)
+        filterRef.current.reactWrapper.querySelector("path")
+            .classList.remove("header__filter-icon-hovered")
+    }
+
     return (
         <nav className="header">
             <a className="header__logo" href="#">Snaps</a>
-            <button className="header__filter-btn">
+            <button 
+                onMouseOver={handleMouseOver} 
+                onMouseOut={handleMouseOut}
+                className={isHovered ? "header__filter-btn header__filter-btn-hovered" : "header__filter-btn"}
+            >
                 Filters
-                <img className="header__filter-icon" src={filter} alt="Filter icon" />
+                <ReactSVG 
+                    src={filterIcon} 
+                    ref={filterRef}
+                />
             </button>
         </nav>
     )

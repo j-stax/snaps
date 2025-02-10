ed20bfc984ea912db0f482e26c6b2013d3994ec3
j-stax <ahnjames01@gmail.com>
2025-02-09 21:40:14 -0500

    Change React svg plugin from ReactSVG to vite-plugin-svgr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,37 @@
 import './Header.scss'
-import filterIcon from '../assets/images/Filter.svg'
-import { ReactSVG } from "react-svg"
-import { useState, useRef, useEffect } from 'react'
+// import filterIcon from '../assets/images/Filter.svg'
+import FilterIcon from '../assets/images/Filter.svg?react'
+import { useState, forwardRef, useRef} from 'react'
+
+const FilterIconWithRef = forwardRef((props, ref) => {
+    return (
+        <svg ref={ref} {...props}>
+            <FilterIcon />
+        </svg>
+    )
+})
 
 export default function Navbar({ toggle, isOpen }) {
     const [isHovered, setIsHovered] = useState(false)
-    const filterRef = useRef(null)
+    const filterRef = useRef()
 
     function handleMouseOver() {
         setIsHovered(true)
-        const filterElemPath = filterRef.current.reactWrapper.querySelector("path")
+        const filterElemPath = filterRef.current.children[0].children[0]
         filterElemPath.classList.add("header__filter-icon-hovered")
     }
 
     function handleMouseOut() {
         if (!isOpen) {
             setIsHovered(false)
-            filterRef.current.reactWrapper.querySelector("path")
+            filterRef.current.children[0].children[0]
                 .classList.remove("header__filter-icon-hovered")
         }
     }
 
     function handleClick() {
         setIsHovered(true)
-        filterRef.current.reactWrapper.querySelector("path")
+        filterRef.current.reactWrapper.children[0].children[0]
             .classList.toggle("header__filter-icon-hovered")
         toggle()
     }
@@ -38,7 +46,7 @@ export default function Navbar({ toggle, isOpen }) {
                 onClick={toggle}
             >
                 Filters
-                <ReactSVG src={filterIcon} ref={filterRef} />
+                <FilterIconWithRef className="header__filter-icon" ref={filterRef} />
             </button>
         </nav>
     )

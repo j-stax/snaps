7d3d7d2bafad7b402e4e53e950690541d946d62e
j-stax <ahnjames01@gmail.com>
2025-02-11 22:55:23 -0500

    desktop-view formatting - filter closed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import './Footer.scss'
-import facebookIcon from '../assets/images/Facebook.svg'
-import xIcon from '../assets/images/X_twitter.svg'
-import igIcon from '../assets/images/Instagram.svg'
-import pinterestIcon from '../assets/images/Pinterest.svg'
+import FacebookIcon from '../assets/images/Facebook.svg?react'
+import XIcon from '../assets/images/X_twitter.svg?react'
+import IgIcon from '../assets/images/Instagram.svg?react'
+import PinterestIcon from '../assets/images/Pinterest.svg?react'
 
 
 export default function Footer() {
@@ -10,24 +10,34 @@ export default function Footer() {
         <footer className="footer">
             <div className="footer__main">
                 <a className="footer__logo" href="#">Snaps</a>
-                <div className="footer__text-content">
-                    <div className="footer__keywords-container">
-                        <span>For photographers</span>
-                        <span>Hire talent</span>
-                        <span>Inspiration</span>
+                <div className="footer__content-wrapper">
+                    <div className="footer__text-container">
+                        <div className="footer__keywords-container">
+                            <span>For photographers</span>
+                            <span>Hire talent</span>
+                            <span>Inspiration</span>
+                        </div>
+                        <nav className="footer__nav">
+                            <a className="footer__nav-link" href="">About</a>
+                            <a className="footer__nav-link" href="">Careers</a>
+                            <a className="footer__nav-link" href="">Support</a>
+                        </nav>
                     </div>
-                    <nav className="footer__nav">
-                        <a className="footer__nav-link" href="">About</a>
-                        <a className="footer__nav-link" href="">Careers</a>
-                        <a className="footer__nav-link" href="">Support</a>
-                    </nav>
+                    <div className="footer__desktop-socials-wrapper">
+                        <div className="footer__desktop-socials">
+                            <FacebookIcon className="footer__desktop-socials-link" />
+                            <XIcon className="footer__desktop-socials-link" />
+                            <IgIcon className="footer__desktop-socials-link" />
+                            <PinterestIcon className="footer__desktop-socials-link" />
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="footer__socials">
-                <img src={facebookIcon} alt="Facebook icon" />
-                <img src={xIcon} alt="X/Twitter icon" />
-                <img src={igIcon} alt="Instagram icon" />
-                <img src={pinterestIcon} alt="Pinterest icon" />
+                <FacebookIcon />
+                <XIcon />
+                <IgIcon />
+                <PinterestIcon />
             </div>
             <div className="footer__legal-text-container">
                 <span>&copy; 2024 Snaps</span>

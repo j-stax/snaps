d6c35eba3a392f582aa94ccc43d8c94c124fcc20
j-stax <ahnjames01@gmail.com>
2025-02-13 16:59:40 -0500

    Header.jsx - add comments; Filters.scss - adjust padding.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import './Header.scss'
 import FilterIcon from '../assets/images/Filter.svg?react'
 import { useState, forwardRef, useRef} from 'react'
 
+// Wrap the FilterIcon to be able to manipulate its properties on events
 const FilterIconWithRef = forwardRef((props, ref) => {
     return (
         <svg ref={ref} {...props}>
@@ -16,6 +17,7 @@ export default function Header({ toggle, isOpen }) {
 
     function handleMouseOver() {
         setIsHovered(true)
+        // Target the <path> child element of the filter icon svg and change the fill color
         const filterElemPath = filterRef.current.children[0].children[0]
         filterElemPath.classList.add("header__filter-icon-hovered")
     }
@@ -23,6 +25,7 @@ export default function Header({ toggle, isOpen }) {
     function handleMouseOut() {
         if (!isOpen) {
             setIsHovered(false)
+            // Remove hover effect
             filterRef.current.children[0].children[0]
                 .classList.remove("header__filter-icon-hovered")
         }
